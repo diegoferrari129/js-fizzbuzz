33f6d1ed2ac76201659f6d7481411aa5b989b8fa
diego ferrari <167826265+diegoferrari129@users.noreply.github.com>
2024-06-28 01:15:07 +0200

    [UPD] styled multiples of numers 3 and 5

--- js/script.js
+++ js/script.js
@@ -3,11 +3,13 @@ for (let i = 1; i < 100; i++) {
     console.log(i);
     // VERIFICA SE IL NUMERO è MULTIPLO DI 3, 5 O DI ENTRAMBI
     // STAMPA FIZZ PER I MULTIPLI DI 3, BUZZ PER I MULTIPLI DI 5 E FIZZBUZZ PER I MULTIPLI DI ENTRAMBI
-    if (i % 3 == 0) {
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log('FizzBuzz');
+    }
+    else if (i % 3 == 0) {
         console.log('Fizz');
     }
     else if (i % 5 == 0) {
         console.log('Buzz');
     }
-
 }
